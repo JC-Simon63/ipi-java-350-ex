d405cb2e956ef656bcff404ecf819e9e2d6765c7
JC-Simon63 <59725601+JC-Simon63@users.noreply.github.com>
2020-03-02 15:06:55 +0100

    test employe + modif pom.xml

--- src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
+++ src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
@@ -71,4 +71,24 @@ public class EmployeTest {
 //        Then return 0
         Assertions.assertThat(result).isEqualTo(0);
     }
+
+
+    @Test
+    public void testgetPrimeAnnuelleMatriculeBeginWithMAndTempsPartiel1(){
+        double result;
+        double compare;
+
+//        Given employe
+        employe = new Employe();
+        employe.setDateEmbauche(LocalDate.now().minusYears(2));
+
+//        When Matricule begin with M and Temps partiel is 1.0
+        employe.setMatricule("M00001");
+        employe.setTempsPartiel(1.0);
+        compare = (Entreprise.primeAnnuelleBase() * Entreprise.INDICE_PRIME_MANAGER + Entreprise.PRIME_ANCIENNETE * employe.getNombreAnneeAnciennete()) * employe.getTempsPartiel();
+        result = employe.getPrimeAnnuelle();
+
+//        Then return 0
+        Assertions.assertThat(result).isEqualTo(compare);
+    }
 }
