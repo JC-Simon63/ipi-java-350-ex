a105741b4c1bc4d0d325554d9b40a30b1f5ea020
JC-Simon63 <59725601+JC-Simon63@users.noreply.github.com>
2020-03-03 12:40:22 +0100

    Update EmployeServiceTest.java

--- src/test/java/com/ipiecoles/java/java350/service/EmployeServiceTest.java
+++ src/test/java/com/ipiecoles/java/java350/service/EmployeServiceTest.java
@@ -7,14 +7,13 @@ import com.ipiecoles.java.java350.model.NiveauEtude;
 import com.ipiecoles.java.java350.model.Poste;
 import com.ipiecoles.java.java350.repository.EmployeRepository;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import javax.persistence.EntityExistsException;
 import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.catchThrowable;
@@ -27,6 +26,11 @@ public class EmployeServiceTest {
 
     @Mock
     EmployeRepository employeRepository;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.initMocks(this.getClass());
+    }
 
     @Test
     public void testEmbaucheEmployeSave() throws EmployeException{
@@ -76,6 +80,27 @@ public class EmployeServiceTest {
         // then
         Assertions.assertThat(thrown).isInstanceOf(EmployeException.class)
                 .hasMessageContaining("Limite des 100000 matricules atteinte !");
+    }
+
+    @Test
+    public void testEmbaucheEmployeExceptionEmployePresent() {
+        // given
+        String nom = "Doe";
+        String prenom = "Jane";
+        Poste poste = Poste.TECHNICIEN;
+        NiveauEtude niveauEtude = NiveauEtude.BTS_IUT;
+        Double tempsPartiel = 1.0;
+
+        Employe e1 = new Employe(nom, prenom, "T00346", LocalDate.now(), 1826.45, 1, tempsPartiel);
+
+        Mockito.when(employeRepository.findLastMatricule()).thenReturn("00345");
+        Mockito.when(employeRepository.findByMatricule("T00346")).thenReturn(e1);
+
+        // when
+        Throwable thrown = catchThrowable(() -> employeService.embaucheEmploye(nom, prenom,poste,niveauEtude,tempsPartiel));
+        // then
+        Assertions.assertThat(thrown).isInstanceOf(EntityExistsException.class)
+                .hasMessageContaining("L'employé de matricule T00346 existe déjà en BDD");
     }
 
 }
